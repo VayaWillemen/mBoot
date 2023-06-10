7fed3245abf6d46a1afd7c493829b6be1de3c3f0
Kurt De Vocht <kurt@bluebits.be>
2023-06-10 20:49:57 +0200

    Added toet

--- src/mBoot.py
+++ src/mBoot.py
@@ -10,6 +10,8 @@ AXIS_THROTTLE_UPDOWN = 2
 AXIS_STICK_TWIST= 3
 AXIS_THROTTLE_BUTTONS = 4
 
+BUTTON_STICK_THUMB = 1
+
 joystick_count = pygame.joystick.get_count()
 print("joystick_count:")
 print(joystick_count)
@@ -21,16 +23,27 @@ if joystick_count == 0:
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
+toetSound = pygame.mixer.Sound("toet.wav")
+
+buttonReleased = False
+
 if __name__ == '__main__':
     bot = mBot()
     bot.startWithHID()
     while(1):
 
+        if( joystick.get_button(1) == 0 ):
+                buttonReleased = True
+        else:
+                if( buttonReleased ):
+                        pygame.mixer.Sound.play(toetSound)
+                buttonReleased = False
+
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         pygame.quit()
                         exit()
-                
+
         speed = -joystick.get_axis(AXIS_THROTTLE_UPDOWN)
         turn = joystick.get_axis(AXIS_THROTTLE_BUTTONS)
 
